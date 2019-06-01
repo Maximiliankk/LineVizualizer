90b9cfb0eceaf9fa16096a3675c72a7151e9783a
Max Klassen <maximiliankk@gmail.com>
2019-06-01 12:28:01 -0700

    Draw and delete buttons

--- Assets/ASM.cs
+++ Assets/ASM.cs
@@ -16,6 +16,13 @@ public class ASM : MonoBehaviour
 {
     List<GameObject> PointList = new List<GameObject>();
     List<GameObject> LineList = new List<GameObject>();
+
+    List<GameObject> TriadPoints = new List<GameObject>();
+    List<GameObject> TriadLines = new List<GameObject>();
+
+    List<GameObject> GridPoints = new List<GameObject>();
+    List<GameObject> GridLines = new List<GameObject>();
+
     public GameObject linePrefab;
     public GameObject pointPrefab;
 
@@ -23,12 +30,12 @@ public class ASM : MonoBehaviour
     public Vector3 cameraFocus = new Vector3(1.5f, 1.5f, 1.5f);
 
     const float defaultLineWidth = 0.2f;
-    const float defaultCameraAngleX = 45;
-    const float defaultCameraAngleY = 45;
-    public float cameraXangle = defaultCameraAngleX;
-    public float cameraYangle = defaultCameraAngleY;
-    private const float defaultCamDistance = 10;
-    private float camDistance = defaultCamDistance;
+    public float defaultCameraAngleX = 45;
+    public float defaultCameraAngleY = 45;
+    public float defaultCamDistance = 10;
+    private float cameraXangle;
+    private float cameraYangle;
+    private float camDistance;
 
     bool oneSelected = false;
     GameObject selectedPoint;
@@ -47,6 +54,7 @@ public class ASM : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        ResetCamera();
         pointLabelStyle.normal.textColor = Color.red;
         menuLabelStyle.normal.textColor = Color.black;
 
@@ -57,68 +65,56 @@ public class ASM : MonoBehaviour
 
         // grid points
 
+
+    }
+
+    void drawGridLines(int dim, float scale, Color c)
+    {
         // grid lines 
         var P0 = new Vector3(0, 0, 0);
         var P1 = new Vector3(0, 0, 0);
 
         // draw 16 lines of the grid along x axis
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < dim; j++)
         {
             P0 = new Vector3(0, j, 0);
-            P1 = new Vector3(3, j, 0);
-            AddLine(P0, P1, gridLinesThickness, Color.red);
-            P0.z += 1; P1.z += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.red);
-            P0.z += 1; P1.z += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.red);
-            P0.z += 1; P1.z += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.red);
-        }
-        // draw 16 lines of the grid along z axis
-        for (int j = 0; j < 4; j++)
-        {
-            P0 = new Vector3(0, j, 0);
-            P1 = new Vector3(0, j, 3);
-            AddLine(P0, P1, gridLinesThickness, Color.blue);
-            P0.x += 1; P1.x += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.blue);
-            P0.x += 1; P1.x += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.blue);
-            P0.x += 1; P1.x += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.blue);
-        }
-        // draw 16 lines of the grid along y axis
-        for (int j = 0; j < 4; j++)
-        {
-            P0 = new Vector3(0, 0, j);
-            P1 = new Vector3(0, 3, j);
-            AddLine(P0, P1, gridLinesThickness, Color.green);
-            P0.x += 1; P1.x += 1;   
-            AddLine(P0, P1, gridLinesThickness, Color.green);
-            P0.x += 1; P1.x += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.green);
-            P0.x += 1; P1.x += 1;
-            AddLine(P0, P1, gridLinesThickness, Color.green);
-        }
-        // draw the grid (small) spheres
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
+            P1 = new Vector3(dim - 1, j, 0);
+
+            for (int k = 0; k < dim; k++)
             {
-                for (int k = 0; k < 4; k++)
-                {
-                    var go = Instantiate(pointPrefab);
-                    go.transform.position = new Vector3(i, j, k);
-                    go.transform.localScale *= defaultLineWidth;
-                    go.transform.GetComponent<Renderer>().material.color = Color.black;
-                }
+                AddLine(P0, P1, gridLinesThickness, c, GridLines);
+                P0.z += 1; P1.z += 1;
             }
         }
-    }
-    
+        for (int j = 0; j < dim; j++)
+        {
+            P0 = new Vector3(0, j, 0);
+            P1 = new Vector3(0, j, dim - 1);
 
-    void drawGrid(int dim, float scale, Color c) {
-	// scale is multiplier
+            for (int k = 0; k < dim; k++)
+            {
+                AddLine(P0, P1, gridLinesThickness, c, GridLines);
+                P0.x += 1; P1.x += 1;
+            }
+        }
+        for (int j = 0; j < dim; j++)
+        {
+            P0 = new Vector3(0, 0, j);
+            P1 = new Vector3(0, dim - 1, j);
+
+            for (int k = 0; k < dim; k++)
+            {
+                AddLine(P0, P1, gridLinesThickness, c, GridLines);
+                P0.x += 1; P1.x += 1;
+            }
+        }
+        // points
+        drawGridPoints(dim, scale, Color.black);
+    }
+
+    // scale is multiplier
+    void drawGridPoints(int dim, float scale, Color c)
+    {
         for (int i = 0; i < dim; i++)
         {
             for (int j = 0; j < dim; j++)
@@ -129,23 +125,25 @@ public class ASM : MonoBehaviour
                     go.transform.position = new Vector3(i, j, k);
                     go.transform.localScale *= defaultLineWidth * scale;
                     go.transform.GetComponent<Renderer>().material.color = c;
+                    GridPoints.Add(go);
                 }
             }
         }
     }
 
     // Makes a sphere and a line (by default)
-    void createPoint(float x, float y, float z, bool makeLine = true)
+    void createPoint(float x, float y, float z, bool makeLine, List<GameObject> pointbuffer, List<GameObject> linebuffer)
     {
         GameObject go = Instantiate(pointPrefab);
         go.transform.position = new Vector3(x - 1, y - 1, z - 1);
         go.transform.GetComponent<Renderer>().material.color = Color.black;
         go.GetComponent<Renderer>().material.color = currentColor;
+
+        pointbuffer.Add(go);
         if (makeLine)
-        {
-            AddLine(go.transform.position, PointList[PointList.Count - 1].transform.position, defaultLineWidth * 2, currentColor);
+        { 
+            AddLine(go.transform.position, pointbuffer[pointbuffer.Count - 1].transform.position, defaultLineWidth * 2, currentColor, linebuffer);
         }
-        PointList.Add(go);
     }
 
     void SetColor(Color c)
@@ -153,69 +151,72 @@ public class ASM : MonoBehaviour
         currentColor = c;
     }
 
-    void DrawFirstPath()
-    {
-        SetColor(Color.black); // color of this path
-        createPoint(4, 3, 1, false);
-        createPoint(4, 2, 2);
-        createPoint(4, 1, 3);
-        createPoint(4, 1, 4);
-        createPoint(3, 1, 4);
-        createPoint(2, 2, 4);
-        createPoint(1, 3, 4);
-        createPoint(2, 3, 4);
-        createPoint(3, 2, 4);
-        createPoint(4, 2, 4);
-        createPoint(4, 2, 3);
-        createPoint(3, 2, 3);
-        createPoint(2, 3, 3);
-        createPoint(3, 3, 3);
-        createPoint(3, 3, 4);
-        createPoint(4, 3, 4);
-        createPoint(4, 3, 3);
-        createPoint(4, 3, 2);
-        createPoint(3, 3, 2);
-        createPoint(2, 4, 2);
-        createPoint(2, 4, 3);
-        createPoint(1, 4, 3);
-        createPoint(1, 4, 4);
-        createPoint(2, 4, 4);
-        createPoint(3, 4, 4);
-        createPoint(3, 4, 3);
-        createPoint(4, 4, 3);
-        createPoint(4, 4, 2);
-        createPoint(3, 4, 2);
-        createPoint(3, 4, 1);
-        createPoint(4, 4, 1);
-    }
+    //void DrawFirstPath()
+    //{
+    //    SetColor(Color.black); // color of this path
+    //    createPoint(4, 3, 1, false);
+    //    createPoint(4, 2, 2, true, TriadLines, TriadPoints);
+    //    createPoint(4, 1, 3);
+    //    createPoint(4, 1, 4);
+    //    createPoint(3, 1, 4);
+    //    createPoint(2, 2, 4);
+    //    createPoint(1, 3, 4);
+    //    createPoint(2, 3, 4);
+    //    createPoint(3, 2, 4);
+    //    createPoint(4, 2, 4);
+    //    createPoint(4, 2, 3);
+    //    createPoint(3, 2, 3);
+    //    createPoint(2, 3, 3);
+    //    createPoint(3, 3, 3);
+    //    createPoint(3, 3, 4);
+    //    createPoint(4, 3, 4);
+    //    createPoint(4, 3, 3);
+    //    createPoint(4, 3, 2);
+    //    createPoint(3, 3, 2);
+    //    createPoint(2, 4, 2);
+    //    createPoint(2, 4, 3);
+    //    createPoint(1, 4, 3);
+    //    createPoint(1, 4, 4);
+    //    createPoint(2, 4, 4);
+    //    createPoint(3, 4, 4);
+    //    createPoint(3, 4, 3);
+    //    createPoint(4, 4, 3);
+    //    createPoint(4, 4, 2);
+    //    createPoint(3, 4, 2);
+    //    createPoint(3, 4, 1);
+    //    createPoint(4, 4, 1);
+    //}
 
     void DrawTriadSet()
     {
         SetColor(Color.green); // color of this path
-        createPoint(6, 3, 3, false);
-        createPoint(3, 6, 3);
-        createPoint(3, 3, 6);
-        createPoint(4, 4, 4);
-        createPoint(5, 3, 4);
-        createPoint(5, 4, 3);
-        createPoint(4, 5, 3);
-        createPoint(3, 5, 4);
-        createPoint(3, 4, 5);
-        createPoint(4, 3, 5);
+        createPoint(6, 3, 3, false, TriadLines, TriadPoints);
+        createPoint(3, 6, 3, true, TriadLines, TriadPoints);
+        createPoint(3, 3, 6, true, TriadLines, TriadPoints);
+        createPoint(4, 4, 4, true, TriadLines, TriadPoints);
+        createPoint(5, 3, 4, true, TriadLines, TriadPoints);
+        createPoint(5, 4, 3, true, TriadLines, TriadPoints);
+        createPoint(4, 5, 3, true, TriadLines, TriadPoints);
+        createPoint(3, 5, 4, true, TriadLines, TriadPoints);
+        createPoint(3, 4, 5, true, TriadLines, TriadPoints);
+        createPoint(4, 3, 5, true, TriadLines, TriadPoints);
     }
 
     void ClearPointsAndLines()
     {
-        foreach (var p in PointList)
-        {
-            Destroy(p);
-        }
-        foreach (var l in LineList)
-        {
-            Destroy(l);
-        }
+        ClearBuffer(PointList);
+        ClearBuffer(LineList);
         PointList.Clear();
         LineList.Clear();
+    }
+
+    void ClearBuffer(List<GameObject> buffer)
+    {
+        foreach(var go in buffer)
+        {
+            Destroy(go);
+        }
+        buffer.Clear();
     }
 
     // Update is called once per frame
@@ -292,14 +293,17 @@ public class ASM : MonoBehaviour
     // creates a cylinder in the scene connecting p1 to p2
     // of thickness linewidth
     // and color lineColor
-    public void AddLine(Vector3 p1, Vector3 p2, float linewidth = defaultLineWidth, Color lineColor = default(Color))
+    public void AddLine(Vector3 p1, Vector3 p2, float linewidth = defaultLineWidth, Color lineColor = default(Color), List<GameObject> buffer = null)
     {
         var go = Instantiate(linePrefab);
         go.transform.position = p1;
         go.transform.localScale = new Vector3(go.transform.localScale.x * linewidth, go.transform.localScale.y * linewidth, Vector3.Distance(p1, p2));
         go.transform.GetComponentInChildren<Renderer>().material.color = lineColor;
         go.transform.LookAt(p2);
-        LineList.Add(go);
+        if(buffer != null)
+        {
+            buffer.Add(go);
+        }
     }
     // toggles the UI control buttons on and off
     private void ToggleControls()
@@ -351,21 +355,31 @@ public class ASM : MonoBehaviour
                 ClearPointsAndLines();
             }
             LeftUIypos += 20;
-            if (GUI.Button(new Rect(10, LeftUIypos, buttonWidths, 20), "Draw First Path"))
-            {
-                StopAllCoroutines();
-                DrawFirstPath();
-            }
+            //if (GUI.Button(new Rect(10, LeftUIypos, buttonWidths, 20), "Draw First Path"))
+            //{
+            //    StopAllCoroutines();
+            //    DrawFirstPath();
+            //}
             LeftUIypos += 20;
             if (GUI.Button(new Rect(10, LeftUIypos, buttonWidths, 20), "Draw Triad Set"))
             {
                 StopAllCoroutines();
                 DrawTriadSet();
             }
+            if (GUI.Button(new Rect(10 + buttonWidths + 10, LeftUIypos, buttonWidths, 20), "Delete Triad Set"))
+            {
+                ClearBuffer(TriadLines);
+                ClearBuffer(TriadPoints);
+            }
             LeftUIypos += 20;
             if (GUI.Button(new Rect(10, LeftUIypos, buttonWidths, 20), "Draw 6x6 cube"))
             {
-		 drawGrid(6, 1, Color.black);
+                drawGridLines(6, 0.5f, new Color(1, 1, 1) * 0.7f);
+            }
+            if (GUI.Button(new Rect(10 + buttonWidths + 10, LeftUIypos, buttonWidths, 20), "Delete 6x6 cube"))
+            {
+                ClearBuffer(GridLines);
+                ClearBuffer(GridPoints);
             }
         }
         if (toggleLabels)
